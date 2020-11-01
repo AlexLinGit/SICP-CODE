18217f110a3da82c8f2ca21ca9fe7c1f64d039e8
AlexLinGit <53539976+AlexLinGit@users.noreply.github.com>
2020-11-01 17:08:12 -0500

    Delete greet_user.py

--- Python Crash Course Exercises/greet_user.py
+++
@@ -1,9 +0,0 @@
-def greet_users(names):
-	"""Greets users in a list"""
-	for name in names:
-		msg = "Hi " + name.title() + "!"
-		print(msg)
-		
-
-usernames = ['alex', 'noah', 'jon', 'angelo']		
-greet_users(usernames)
