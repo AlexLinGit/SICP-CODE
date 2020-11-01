194b119ff1fcf9e4952753cb5cf3eb9d917df980
AlexLinGit <53539976+AlexLinGit@users.noreply.github.com>
2020-11-01 17:09:11 -0500

    Delete sandwich.py

--- Python Crash Course Exercises/sandwich.py
+++
@@ -1,9 +0,0 @@
-def make_sandwich(*ingrediants):
-	"""Summarizes sub ingrediants"""
-	print("\nSub ingrediants: ")
-	for ingrediant in ingrediants:
-		print("- " + ingrediant)
-		
-
-make_sandwich('turkey', 'cheese')
-make_sandwich('beef', 'cheese', 'mushrooms')
